3c118acac213f5f7884db03d5dbf7e106b1433a5
Florian Brüggemann <look@cheesyeyes.com>
2016-11-28 14:02:42 +0100

    hello space

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import EventEmitter from 'events';
+
+class Events {
+  constructor(){
+    this.events = {};
+  }
+
+  on(eventName, callback){
+    if(this.events[eventName]){
+      this.events[eventName].push(callback);
+    }else{
+      this.events[eventName] = [callback];
+    }
+  }
+
+  go(eventName, ...rest){
+    if(this.events[eventName]){
+      this.events[eventName].forEach(cb =>{
+        cb(...rest);
+      })
+    }
+  }
+}
+
+const ee = new Events();
+
+ee.on('push', (name) => {
+  console.log(name+' you moose!');
+});
+
+class Thing extends EventEmitter{
+  constructor(){
+    super();
+    this.set = {
+      title : function(title){
+        return console.log('listen to '+title);
+      },
+      subtitle : function(title){
+        return console.log('listen to '+title);
+      }
+    }
+  }
+}
+
+let guy = new Thing();
+guy.set.title('big guy');
+guy.set.subtitle('yellow tree');
+
+addEventListener('mouseup', function(){
+  ee.go('push', 'Lennon');
+});
